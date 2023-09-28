03bd2c0fe9169977c96e1a615de9245d4decf917
fiap-user-ads <126879241+fiap-user-ads@users.noreply.github.com>
2023-09-28 19:08:47 -0300

    update

--- atividade-cp-2.py
+++ atividade-cp-2.py
@@ -2,16 +2,48 @@
 # RM: 550371
 # TURMA: 1TDSPN
 
-import os
-
-# ================ SUBALGORITMOS 
+# ================ SUBALGORITMOS
 
 def verifica_email(email: str)  -> None:
-    errado()
-    print("Func")
+    partes = email.split("@")
 
-def correto() -> None:
-    print("Correto")
+    if len(partes) != 2:
+        errado()
+        return
+
+    parte_1_nome = partes[0]
+    parte_2_dominio = partes[1]
+
+    caracteres_especiais = "!@#$%^&*()_+{}[]:;<>,.?~\\|/"
+
+    for caracter in parte_1_nome:
+        if caracter in caracteres_especiais:
+            errado()
+            return
+
+    if parte_2_dominio.count(".") < 1 or parte_2_dominio.count(".") > 2:
+        errado()
+        return
+    if ".." in parte_2_dominio:
+        errado()
+        return
+    if parte_2_dominio.index(".") == 0:
+        errado()
+        return
+    if parte_2_dominio.count(".") < 1:
+        errado()
+        return
+    if parte_2_dominio[-1] == ".":
+        errado()
+        return
+
+    correto(email)
+    return True
+
+def correto(email: str) -> None:
+    arq = open("RM550371.txt", "w", encoding="utf-8")
+    arq.write(email)
+    arq.close()
 
 def errado() -> None:
     print("E-mail inválido, digite outro correto!")
@@ -19,9 +51,9 @@ def errado() -> None:
 # ================ PROGRAMA PRINCIPAL
 
 while True:
-    os.system("cls")
-    
     email = input("Digite o e-mail: ")
-    verifica_email(email) 
+    registro = verifica_email(email)
 
-    
+    if registro == True:
+        print("\n Gravado com sucesso!")
+        break
